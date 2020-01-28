eb2df7ac50a88c681c7e971cc2c230e89d40e809
Carmen-Alina <carmen.panu@gmail.com>
2020-01-28 16:54:21 +0000

    User story 1 - wip

---
+++ spec/AirportSpec.js
@@ -0,0 +1,14 @@
+'use strict';
+describe('Airport', function(){
+  var airport;
+  beforeEach(function(){
+    airport = new Airport();
+  });
+  it('has no planes by default', function(){
+    expect(airport.planes()).toEqual([]);
+  });
+  it('can clear planes for landing', function(){
+    airport.clearForLanding(plane);
+    expect(airport.planes()).toEqual([plane]);
+  });
+});
